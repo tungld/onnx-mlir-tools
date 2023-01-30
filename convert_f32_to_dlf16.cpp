5b1809f34f0a0a838212845a3912a3e7125c9508
Tung D. Le <tung@jp.ibm.com>
2023-01-30 16:29:26 +0900

    Add a single code for fp32-to-dlf16
    Signed-off-by: Tung D. Le <tung@jp.ibm.com>

--- convert_f32_to_dlf16.cpp
+++ convert_f32_to_dlf16.cpp
@@ -10,6 +10,7 @@
 #include <cstdint>
 #include <cstdio>
 #include <cstring>
+#include <cassert>
 
 // Macros for FPFormat.
 #define SIGN(one, exp, frac) (one << exp << frac)
@@ -34,10 +35,11 @@ int main(int argc, char *argv[]) {
   // printf("z: %f\n", z);
 
   //--------------------------------------------------------------------------//
-  // dlf16 to f32
+  // f32 to dlf16
   //--------------------------------------------------------------------------//
-  uint16_t dlf16_item = atoi(argv[1]);
-  float fp;
+  float fp = atof(argv[1]); // input 0.0123456789
+  // assert((fp == 0.0123456789f) && "please input 0.0123456789");
+  uint16_t dlf16_item; // expected output: 12585
 
   // from constructor of parent FPFormat: `public FPFormat<uint16_t, 6, 9>`
   static constexpr unsigned DLF16_EXPONENT_BITS = 6;
@@ -65,21 +67,43 @@ int main(int argc, char *argv[]) {
       EXPO_BIAS(FP32_One, FP32_EXPONENT_BITS, FP32_FRACTION_BITS);
   static constexpr uint32_t FP32_FRACTION = FRAC(FP32_One, FP32_FRACTION_BITS);
 
-  if ((dlf16_item & ~DLF16_SIGN) == 0) {
-    fp = ((dlf16_item & DLF16_SIGN) == 0) ? +0.0f : -0.0f;
-  } else if ((dlf16_item & ~DLF16_SIGN) == DLF16_NINF) {
-    fp = NAN;
-  } else {
-    uint32_t fp32 = FP32_SIGN * BFGET(dlf16_item, DLF16_SIGN);
-    BFPUT(fp32, FP32_EXPONENT,
-        BFGET(dlf16_item, DLF16_EXPONENT) - DLF16_EXPONENT_BIAS +
-            FP32_EXPONENT_BIAS);
-    BFPUT(fp32, FP32_FRACTION,
-        BFGET(dlf16_item, DLF16_FRACTION)
-            << (FP32_FRACTION_BITS - DLF16_FRACTION_BITS));
-    memcpy(&fp, &fp32, sizeof(fp));
+  static constexpr uint32_t FP32_DLF16_ROUND =
+      1 << (FP32_FRACTION_BITS - DLF16_FRACTION_BITS - 1);
+
+  static constexpr uint32_t FP32_DLF16_NMAX =
+      (((1 << DLF16_EXPONENT_BITS) - 1 + FP32_EXPONENT_BIAS -
+        DLF16_EXPONENT_BIAS)
+       << FP32_FRACTION_BITS) |
+      (((1 << DLF16_FRACTION_BITS) - 2)
+       << (FP32_FRACTION_BITS - DLF16_FRACTION_BITS)) |
+      (FP32_DLF16_ROUND - 1);
+
+  // Conversion.
+  uint32_t fp32;
+  memcpy(&fp32, &fp, sizeof(fp32));
+
+  signed nnp1_biased_exponent =
+      BFGET(fp32, FP32_EXPONENT) - FP32_EXPONENT_BIAS + DLF16_EXPONENT_BIAS;
+  uint32_t fraction = BFGET(fp32, FP32_FRACTION) + FP32_DLF16_ROUND;
+
+  if (fraction > FP32_FRACTION) {
+    fraction = 0;
+    nnp1_biased_exponent++;
   }
 
-  // printf("fp: %f\n", fp); // expected result: 0.012344 
-  return (int)fp; 
+  uint16_t uint = DLF16_SIGN * BFGET(fp32, FP32_SIGN);
+
+  if (nnp1_biased_exponent >= 0) {
+    if ((fp32 & ~FP32_SIGN) <= FP32_DLF16_NMAX) {
+      BFPUT(uint, DLF16_EXPONENT, nnp1_biased_exponent);
+      BFPUT(uint, DLF16_FRACTION,
+            fraction >> (FP32_FRACTION_BITS - DLF16_FRACTION_BITS));
+    } else {
+      uint |= DLF16_NINF;
+    }
+  }
+
+  // assert(uint == 12585);
+  // printf("dlf16 in uint: %i\n", uint); // expected result: 12585
+  return uint;
 }
